ba73c0bdf57d99b5e1e15c67ee7237603d3d0ca7
Frode <frodeluren@gmail.com>
2019-10-29 12:02:22 +0100

    Fixes issue where header overlaps drawer menu

--- app/src/main/java/net/cryptofile/app/MainActivity.java
+++ app/src/main/java/net/cryptofile/app/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.Menu;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
