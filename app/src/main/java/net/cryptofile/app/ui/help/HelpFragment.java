88745305b0719f6ecc37c8f1e92e2e679c1af612
Emival08 <zordaso@gmail.com>
2019-10-29 13:04:58 +0100

    Added help in navigation drawer

--- app/src/main/java/net/cryptofile/app/ui/help/HelpFragment.java
+++ app/src/main/java/net/cryptofile/app/ui/help/HelpFragment.java
@@ -1,4 +1,4 @@
-package net.cryptofile.app.ui.share;
+package net.cryptofile.app.ui.help;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import net.cryptofile.app.R;
 
-public class ShareFragment extends Fragment {
+public class HelpFragment extends Fragment {
 
-    private ShareViewModel shareViewModel;
+    private HelpViewModel helpViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        shareViewModel =
-                ViewModelProviders.of(this).get(ShareViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_share, container, false);
-        final TextView textView = root.findViewById(R.id.text_share);
-        shareViewModel.getText().observe(this, new Observer<String>() {
+        helpViewModel =
+                ViewModelProviders.of(this).get(HelpViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_help, container, false);
+        final TextView textView = root.findViewById(R.id.text_help);
+        helpViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/net/cryptofile/app/ui/help/HelpViewModel.java
+++ app/src/main/java/net/cryptofile/app/ui/help/HelpViewModel.java
@@ -1,16 +1,16 @@
-package net.cryptofile.app.ui.share;
+package net.cryptofile.app.ui.help;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class ShareViewModel extends ViewModel {
+public class HelpViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public ShareViewModel() {
+    public HelpViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is share fragment");
+        mText.setValue("Want help? Ask your mother!");
     }
 
     public LiveData<String> getText() {
