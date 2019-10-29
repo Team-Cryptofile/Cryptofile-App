a4b14cd0c130cf67e427d776e109beb4873891b5
Frode <frodeluren@gmail.com>
2019-10-29 15:40:21 +0100

    Auto stash before merge of "FileList" and "origin/development"

--- app/src/main/java/net/cryptofile/app/ui/home/HomeViewModel.java
+++ app/src/main/java/net/cryptofile/app/ui/home/HomeViewModel.java
@@ -1,19 +1,56 @@
 package net.cryptofile.app.ui.home;
 
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import net.cryptofile.app.dummy.DummyContent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+public class HomeViewModel extends AndroidViewModel {
 
-    public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+    MutableLiveData<List<DummyContent.DummyItem>> fileList;
+    MutableLiveData<DummyContent.DummyItem> selected = new MutableLiveData<>();
+
+    RequestQueue requestQueue;
+
+    public HomeViewModel(Application context) {
+        super(context);
+        requestQueue = Volley.newRequestQueue(context);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    LiveData<List<DummyContent.DummyItem>> getFileList() {
+        if(fileList == null) {
+            fileList = new MutableLiveData<>();
+            loadFileList();
+        }
+
+        return fileList;
     }
+
+    public LiveData<DummyContent.DummyItem> getSelected() {
+        return selected;
+    }
+
+    protected void loadFileList() {
+        // TODO: 29.10.2019 Load file list from local database
+        List<DummyContent.DummyItem> fileList = new ArrayList<>();
+        DummyContent dummy = new DummyContent();
+
+        fileList.add(dummy.createDummyItem(1));
+        fileList.add(dummy.createDummyItem(2));
+        fileList.add(dummy.createDummyItem(3));
+        fileList.add(dummy.createDummyItem(4));
+
+        this.fileList.setValue(fileList);
+    }
+
 }
