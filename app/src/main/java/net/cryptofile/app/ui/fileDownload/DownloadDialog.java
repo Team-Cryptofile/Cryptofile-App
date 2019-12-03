d0c857ff5b34a5f0ab09b1e8c70c2dc5159f4ef8
Frode <frodeluren@gmail.com>
2019-12-03 17:18:30 +0100

    Download input can recieve UUID and key in one
    Decryption is not fworking yet.
    Adds downloaded files to filelist, and keys to kelist.

--- app/src/main/java/net/cryptofile/app/ui/fileDownload/DownloadDialog.java
+++ app/src/main/java/net/cryptofile/app/ui/fileDownload/DownloadDialog.java
@@ -13,11 +13,25 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import net.cryptofile.app.R;
+import net.cryptofile.app.data.CryptoService;
+import net.cryptofile.app.data.FileService;
+import net.cryptofile.app.data.Result;
 
+import org.apache.tika.io.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
@@ -49,20 +63,63 @@ public class DownloadDialog extends AppCompatDialogFragment {
     }
 
 
-    private void downloadFile(String uuid) {
+    public void downloadFile(String uuidInput) {
+        boolean isOnlyUuid = uuidInput.matches("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
+        boolean isUuidWithKey= uuidInput.matches("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}:.+=$");
         Thread thread = new Thread(() -> {
             try {
                 String urlPath = "http://cryptofile.net:8080/get/";
-                if(uuid != null) {
+                if(isOnlyUuid) {    // This runs if UUID is valid but have no key
+                    URL url = new URL(urlPath + uuidInput);
+
+                    // Grabbing file title from server
+                    String title = "";
+                    URL urlTitle = new URL(urlPath + "title/" + uuidInput);
+                    HttpURLConnection c = (HttpURLConnection) urlTitle.openConnection();
+                    if (c.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                        title = c.getResponseMessage();
+                    } else {
+                        System.out.println("Could not get the title: " + c.getResponseMessage());
+                    }
+                    c.disconnect();
+
+                    FileService.addFile(uuidInput, title);
+                } else if (isUuidWithKey) { // This runs if there is a UUID and a key attached to it
+                    CryptoService.saveKey(uuidInput);
+
+                    String[] split = uuidInput.split(":", 2);
+                    String uuid = split[0];
+
+
+                    // Grabbing file title from server
+                    String title = "";
+                    URL urlTitle = new URL(urlPath + "title/" + uuid);
+                    System.out.println("URL: " + urlTitle);
+                    HttpURLConnection c = (HttpURLConnection) urlTitle.openConnection();
+                    if (c.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                        title = c.getResponseMessage();
+                        System.out.println("Recieved title: " + title);
+                    } else {
+                        System.out.println("Could not get the title: " + c.getResponseMessage());
+                    }
+                    c.disconnect();
+
+                    // Adds file to filelist
+                    FileService.addFile(uuid, title);
+
+                    // Getting the file itself
                     URL url = new URL(urlPath + uuid);
                     InputStream in = url.openStream();
-                    Files.copy(in, Paths.get(uuid), StandardCopyOption.REPLACE_EXISTING);
-                    System.out.println(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS));
+                    Path filePath =  Paths.get(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath(), uuid);
+
+                    byte[] decryptedBytes = CryptoService.decrypt(CryptoService.getKey(uuid), IOUtils.toByteArray(in));
+                    ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(decryptedBytes);
+                    Files.copy(byteArrayInputStream, filePath);
                     in.close();
                 }
 
 
-            } catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         });
